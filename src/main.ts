4a96e99af4403ba7ae191ce9b44d3e87b5182aaf
charlieScript <gozione3@gmail.com>
2022-04-07 18:50:39 +0100

    fixed port

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   // app.use(passport.initialize());
   // app.use(passport.session());
-
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
