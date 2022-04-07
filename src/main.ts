6688bb8b64445b2df4ee541b9d54a54283ce5496
charlieScript <gozione3@gmail.com>
2022-04-07 19:00:59 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,6 @@ import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-
-  // app.use(session({
-  //   secret: 'secret',
-  //   resave: false,
-  //   saveUninitialized: true,
-  //   cookie: { maxAge: 60000 }
-  // }));
-
-  // app.use(passport.initialize());
-  // app.use(passport.session());
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
